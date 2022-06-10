d57201851b133f9a5f904c9e5e59b7c6a915044f
artemsav <studentbedn@gmail.com>
2022-06-10 15:41:11 +0300

    Add data validation for register_order.

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -5,6 +5,7 @@ import json
 from .models import Product, Order, OrderItem
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 def banners_list_api(request):
     # FIXME move data to db?
@@ -61,18 +62,26 @@ def product_list_api(request):
 @api_view(['POST'])
 def register_order(request):
     response = request.data
-    order = Order.objects.create(
-        address=response.get('address'),
-        name=response.get('firstname'),
-        surname=response.get('lastname'),
-        phone_number=response.get('phonenumber')
-    )
-    products = Product.objects.all()
-    for item in response.get('products'):
-        OrderItem.objects.create(
-            order=order,
-            quantity=item.get('quantity'),
-            product=products.get(id=item.get('product'))
-        )
-    print(response)
-    return Response(response)
+    if isinstance(response.get('products'), list):
+        if len(response.get('products')) > 0:
+            order = Order.objects.create(
+                address=response.get('address'),
+                name=response.get('firstname'),
+                surname=response.get('lastname'),
+                phone_number=response.get('phonenumber')
+                )
+            products = Product.objects.all()
+            for item in response.get('products'):
+                OrderItem.objects.create(
+                    order=order,
+                    quantity=item.get('quantity'),
+                    product=products.get(id=item.get('product'))
+                )
+            print(response)
+            return Response(response)
+        else:
+            content = {'data Erorr': 'products key not presented or not list'}
+            return Response(content, status=status.HTTP_200_OK)
+    else:
+        content = {'data Erorr': 'products key not presented or not list'}
+        return Response(content, status=status.HTTP_200_OK)
