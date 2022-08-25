68cff53afc7cb001e7e44a348446a2bf7f8856ff
artemsav <studentbedn@gmail.com>
2022-08-25 10:58:19 +0300

    move geo functions to geoapp

--- geoapp/views.py
+++ geoapp/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+import requests
+from geopy import distance
 
-# Create your views here.
+
+def get_distance(coordinates, restaurant_coordinates):
+    lan, lot = coordinates
+    restaurant_lan, restaurant_lot = restaurant_coordinates
+    if lan and lot and restaurant_lan and restaurant_lot is not None:
+        return distance.distance(coordinates, restaurant_coordinates).km
+
+
+def check_address(addresses, saved_addresses):
+    check_result = []
+    for address in addresses:
+        if address in saved_addresses:
+            continue
+        else:
+            check_result.append(address)
+    return check_result
+
+
+def fetch_coordinates(apikey, address):
+    base_url = "https://geocode-maps.yandex.ru/1.x"
+    response = requests.get(base_url, params={
+        "geocode": address,
+        "apikey": apikey,
+        "format": "json",
+    })
+    response.raise_for_status()
+    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
+
+    if not found_places:
+        return None
+
+    most_relevant = found_places[0]
+    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
+    return lon, lat
--- restaurateur/views.py
+++ restaurateur/views.py
@@ -1,54 +1,17 @@
 from contextlib import suppress
 
-import requests
 from django import forms
 from django.conf import settings
 from django.contrib.auth import authenticate, login
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import redirect, render
-from django.template.defaulttags import register
 from django.urls import reverse_lazy
 from django.views import View
-from geopy import distance
-from restaurateur.exceptions import CoordinatesNotFound
+from geoapp.exceptions import CoordinatesNotFound
 from foodcartapp.models import Order, Product, Restaurant
 from geoapp.models import AddressCoordinates
-
-
-def get_distance(coordinates, restaurant_coordinates):
-    lan, lot = coordinates
-    restaurant_lan, restaurant_lot = restaurant_coordinates
-    if lan and lot and restaurant_lan and restaurant_lot is not None:
-        return distance.distance(coordinates, restaurant_coordinates).km
-
-
-def check_address(addresses, saved_addresses):
-    check_result = []
-    for address in addresses:
-        if address in saved_addresses:
-            continue
-        else:
-            check_result.append(address)
-    return check_result
-
-
-def fetch_coordinates(apikey, address):
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-
-    if not found_places:
-        return None
-
-    most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-    return lon, lat
+from geoapp.views import get_distance, fetch_coordinates
 
 
 class Login(forms.Form):
